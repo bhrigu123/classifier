ff52e655d961fbb077a47099b0058992904a1f5b
sachin <iclcoolster@gmail.com>
2016-01-25 18:20:58 +0530

    Added missing bin/config.py

---
+++ bin/config.py
@@ -0,0 +1,15 @@
+name = 'classifier'
+version = '1.5.1'
+
+description = "Organize files in your directory instantly, by classifying \
+them into different folders."
+
+# Formats should be added below with respective directory name.
+formats = {
+    'Music': ['.mp3','.aac','.flac','.ogg','.wma','.m4a','.aiff'],
+    'Videos': ['.flv','.ogv','.avi','.mp4','.mpg','.mpeg','.3gp'],
+    'Pictures': ['.png','.jpeg','.gif','.jpg','.bmp','.svg','webp'],
+    'Archives': ['.rar','.zip','.7z','.tar.gz','.tar.bz2', '.tar'],
+    'Documents': ['.txt', '.pdf','.doc','.docx','.xls','.xlsv','.xlsx',
+                  '.ppt','.pptx','.ppsx','.odp','.odt','.ods']
+}
