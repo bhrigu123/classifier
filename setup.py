9fa53c257a2478ce8a9a402c6d0c0c573b7ae4a6
Yi Tang <ssnailtang@gmail.com>
2016-02-01 11:53:49 +0800

    Fix #43: os path join raise unicode decode error
    In python2, UnicodeDecodeError raised when dir/file name is non-ASCII
    using getcwdu (six provide getcwd to be compatible with Python3)
    to get current dir and passing it to listdir which also return unicode
    and decoding folder-relative args as utf-8
    
    Arrange the structure of unittest, and add a testcase for classifying by
    date which will raise the Error mentioned before

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         """,
         install_requires=[
             'arrow',
+            'six',
         ],
         zip_safe=False
 )
--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,28 +1,59 @@
 # -*- coding: utf-8 -*-
 
+import arrow
 import os
+import shutil
 import unittest
 
 from classifier import classifier as clf
+from six.moves import getcwd
 
 
 class ClassifierTest(unittest.TestCase):
 
     __location = os.path.realpath(
-        os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        os.path.join(getcwd(), os.path.dirname(__file__), '.unittest'))
 
-    __tmp_file_name = '.classifier_test'
+    __tmp_files = [u'test_file', u'test_file_中文']
+    __tmp_dirs = [u'test_dir', u'test_dir_中文']
+
+    def setUp(self):
+        if not os.path.exists(self.__location):
+            os.mkdir(self.__location)
+        os.chdir(self.__location)
+        for file_ in self.__tmp_files:
+            open(file_, 'w').close()
+        for dir_ in self.__tmp_dirs:
+            if not os.path.exists(dir_):
+                os.mkdir(dir_)
+        super(ClassifierTest, self).setUp()
+
+    def tearDown(self):
+        shutil.rmtree(self.__location)
+        super(ClassifierTest, self).tearDown()
 
     def test_moveto(self):
-        os.chdir(self.__location)
-        tmp_file = open(self.__tmp_file_name, 'w')
+        target_dir = os.path.abspath(os.path.join(self.__location, 'moveto'))
+        for file_ in self.__tmp_files:
+            clf.moveto(file_, self.__location, target_dir)
 
-        parent_dir = os.path.abspath(os.path.join(self.__location, os.pardir))
-        clf.moveto(self.__tmp_file_name, self.__location, parent_dir)
+        for file_ in self.__tmp_files:
+            final_file_path = os.path.join(target_dir, file_)
+            self.assertTrue(os.path.exists(final_file_path))
 
-        final_file_path = os.path.join(parent_dir, self.__tmp_file_name)
-        self.assertTrue(os.path.exists(final_file_path))
-        os.remove(final_file_path)
+    def test_classify_bydate(self):
+        date_format = 'DD-MM-YYYY'
+        target_files = []
+        for file_ in self.__tmp_files:
+            target_dir = arrow.get(os.path.getctime(file_)).format(date_format)
+            final_file_path = os.path.join(target_dir, file_)
+            target_files.append(final_file_path)
+        clf.classify_by_date(date_format, self.__location)
+        for file_ in target_files:
+            self.assertTrue(os.path.exists(final_file_path))
+        for dir_ in self.__tmp_dirs:
+            self.assertTrue(os.path.exists(dir_))
+
 
 if __name__ == '__main__':
     unittest.main()
