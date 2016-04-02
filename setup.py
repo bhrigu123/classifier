1b39cfc62991df41f82b9fce145c83d8c7bc4704
Bhrigu Srivastava <captain.bhrigu@gmail.com>
2016-04-02 07:32:46 +0530

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name="classifier",
-        version="1.6.4",
+        version="1.6.5",
         description="Classify the files in your Downloads folder into suitable destinations.",
         url="http://github.com/bhrigu123/classifier",
         author="Bhrigu Srivastava",
